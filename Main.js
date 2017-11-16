abafd9d8e0a2b5b6b1a2eddb5e5678626719298e
sdejnert <dejnertsebastian@gmail.com>
2017-11-16 18:01:45 +0100

    Wersja programu 1.0

--- Main.js
+++ Main.js
@@ -3,14 +3,16 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "931026"
+	password: "931026",
+	database:"mydb"
 });
 
 con.connect(function(err) {
 	if(err) throw err;
 	console.log("Connected!");
-	con.query("CREATE DATABASE mydb", function (err, result) {
+	var sql = "CREATE TABLE customers (id INT(255), product_name VARCHAR(255))";
+	con.query(sql, function (err, result) {
 		if(err) throw err;
-		console.log("Database created");
+		console.log("Table created");
 	});
 });
